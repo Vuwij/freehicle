60c691ad7a12f8996e7395d0b1551d89d2a3cc1c
Jason Wang <vuwij1@gmail.com>
2023-01-01 18:32:44 +0900

    A lot of work done

--- freehicle_control/src/main.py
+++ freehicle_control/src/main.py
@@ -4,31 +4,39 @@ import os
 import time
 from os.path import expanduser
 
+from cv_bridge import CvBridge
 from sensor_msgs.msg import Image, CameraInfo
 
 import pybullet as pb
 import pybullet_data
 import rospy
 
+from soccer_common.transformation import Transformation
+
 if __name__ == "__main__":
     rospy.init_node("freehicle_control")
     pub_camera = rospy.Publisher("camera/image_raw", Image, queue_size=1)
     pub_camera_info = rospy.Publisher("camera/camera_info", CameraInfo, queue_size=1, latch=True)
+    rospy.set_param("/use_sim_time", False)
 
     client_id = pb.connect(pb.GUI)
     pb.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
     pb.setGravity(0, 0, -9.81)
     # pb.configureDebugVisualizer(pb.COV_ENABLE_GUI, 0)
-    plane = pb.loadURDF("plane.urdf", basePosition=[0, 0, 0], baseOrientation=[0, 0, 0, 1])
+    home = expanduser("~")
+
+    plane = pb.loadURDF(
+        home
+        + f"/catkin_ws/src/freehicle/freehicle_description/urdf/parking_lot/parking_lot.urdf"
+        , basePosition=[0, 0, 0], baseOrientation=[0, 0, 0, 1])
     pb.changeDynamics(
         plane,
         linkIndex=-1,
-        lateralFriction=0.9,
-        spinningFriction=0.9,
+        lateralFriction=1.0,
+        spinningFriction=0,
         rollingFriction=0.0
     )
 
-    home = expanduser("~")
     pybullet_id = pb.loadURDF(
         home
         + f"/catkin_ws/src/freehicle/freehicle_description/urdf/freehicle/freehicle.urdf",
@@ -48,39 +56,62 @@ if __name__ == "__main__":
     near = 0.02
     far = 10
 
-    view_matrix = pb.computeViewMatrix([0, 0, 1], [2, 0, 0], [1, 0, 0])
     projection_matrix = pb.computeProjectionMatrixFOV(fov, aspect, near, far)
 
+    t = 0
+    r = rospy.Rate(1 / 0.004)
+    cvbridge = CvBridge()
 
     while(True):
         # Get depth values using the OpenGL renderer
+        camera_position = pb.getLinkState(pybullet_id, 4)[0]
+        camera_angle = pb.getLinkState(pybullet_id, 4)[1]
+        camera_transform = Transformation(camera_position, camera_angle)
+        target_position = Transformation(position=[1, 0, -0.5])
+        target_camera_position = camera_transform @ target_position
+
+        view_matrix = pb.computeViewMatrix(cameraEyePosition=camera_position, cameraTargetPosition=target_camera_position.position, cameraUpVector=[0, 0, 1])
         width, length, rgbPixels, depthPixels, segmentationMaskBuffer = pb.getCameraImage(width, height, view_matrix, projection_matrix, renderer=pb.ER_BULLET_HARDWARE_OPENGL)
 
-        img_msg = Image()
-        img_msg.header.stamp = rospy.Time.now()
-        img_msg.header.frame_id = "/camera"
-        img_msg.height = height
-        img_msg.width = width
-        img_msg.encoding = b"bgra8"
-        img_msg.step = 4 * width
-        img_msg.data = rgbPixels
+        img_msg = cvbridge.cv2_to_imgmsg(rgbPixels)
         pub_camera.publish(img_msg)
 
-        time.sleep(0.01)
+        if t < 1:
+            control = [0, 10]
+        elif t < 2.5:
+            control = [-1, 10]
+        elif t < 3.0:
+            control = [1, 0]
+        elif t < 4.5:
+            control = [1, -3]
+        elif t < 6.0:
+            control = [-0.3, 7]
+        elif t < 8.0:
+            control = [0, 4]
+        elif t < 11.0:
+            control = [0, 0]
 
-        control = [1, 1, 1]
         pb.setJointMotorControlArray(
             bodyIndex=pybullet_id,
             controlMode=pb.VELOCITY_CONTROL,
-            jointIndices=[0, 1, 2, 3, 4, 5],
+            jointIndices=[1, 3, 4, 5],
             targetVelocities=[
-                control[0],
                 control[1],
-                control[0],
                 control[1],
-                control[2],
-                control[2],
+                control[1],
+                control[1],
             ],
-            velocityGains=[0.4] * 6,
+            velocityGains=[0.4] * 4,
+        )
+        pb.setJointMotorControlArray(
+            bodyIndex=pybullet_id,
+            controlMode=pb.POSITION_CONTROL,
+            jointIndices=[0, 2],
+            targetPositions=[
+                control[0],
+                control[0]
+            ]
         )
         pb.stepSimulation()
+        r.sleep()
+        t = t + 0.004
